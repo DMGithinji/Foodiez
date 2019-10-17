3f34e68720b8a965ae755a840712610471d8db67
DMG <dmwaigithinji@gmail.com>
2019-10-18 01:20:21 +0300

    Get all locations functional and filter implemented

--- src/app/components/restaurant-card/restaurant-card.component.ts
+++ src/app/components/restaurant-card/restaurant-card.component.ts
@@ -20,13 +20,12 @@ export class RestaurantCardComponent implements OnInit {
   constructor(private route: ActivatedRoute, private _restaurant: RestaurantService) {}
 
   restaurant = [];
-
+  locations: [];
+  filteredLocations: any[];
   rest: any[];
-  filteredRestaurants: any[];
+  filteredRestaurants: [];
   restrnts
-  locations=[]
-  locationSet;
-  restaurants;
+  restaurants: any[];
 
 
   ngOnInit() {
@@ -41,12 +40,15 @@ export class RestaurantCardComponent implements OnInit {
 
   loadRestaurants = () =>{
     this._restaurant.getRestaurant()
-      .subscribe((res:any[])=> {
+      .subscribe((res:[])=> {
         res => this.restaurant.push(res);
         if(res){
           this.rest = this.restaurant[0].data;
-          this.restrnts = this.rest;   
+          this.restrnts = this.rest; 
+          this.locations = this.restrnts.map((resto)=> resto.location); 
+          this.filteredLocations = this.locations.filter((x, i, a)=> a.indexOf(x) == i);
           console.log(this.restrnts);     
+          console.log("filteredLocations", this.filteredLocations);     
         }
       },
         err => console.log(err)
@@ -61,11 +63,11 @@ export class RestaurantCardComponent implements OnInit {
 
 
   filterByLocation(location) {
+    this.getRest()
     this.filteredRestaurants = this.restrnts.filter(resto => resto.location === location );
-    this.restrnts;
+    this.restrnts = this.filteredRestaurants;
     console.log(this.filteredRestaurants);
+
 }
-
-
 
 }
